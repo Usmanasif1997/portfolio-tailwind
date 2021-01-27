0d654e9d02f7bcaef4b1f1adb24f6c1ce1788cc1
jusmanasif435 <jusmanasif435@gmail.com>
2021-01-27 17:17:33 +0500

    make specialize section

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+
+// Typed Js
+var typed = new Typed('.element', {
+    strings: ['Digital Marketing Expert', 'Full Stack Developer', 'Designer'],
+    typeSpeed: 100,
+    backDelay: 3000,
+    backSpeed: 50,
+    loop: true
+});
+
+
+//  WOW Js
+new WOW().init();
